f89c635f7ae8aade7d9ac12c90c6a07feac6ef58
Furqan Khan <160186238+furqanalikhan1@users.noreply.github.com>
2025-03-13 03:53:22 +0500

    convertor.py

--- convertor.py
+++ convertor.py
@@ -31,4 +31,4 @@ st.write("""
 3️⃣ **Enter a value** and press **Convert** to see the result.  
 """)
 
-st.markdown(f"🔗 [View Source Code on GitHub]({https://github.com/furqanalikhan1/convertor.py/edit/main/convertor.py})")
+st.markdown(f"🔗 [View Source Code on GitHub]({github_repo_url})")
